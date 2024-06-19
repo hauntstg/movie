89739a669b01482deb14b2bc629b69c111e02582
Hau Nguyen <nthau.it96@gmail.com>
2024-06-19 08:06:40 +0700

    Updates

---
+++ precache-manifest.2d622daaabaa2d8c2589a1d6cf871f0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65770c97966e66ba14dadf397c295bc2",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "4900e10750fb74d6d713",
+    "url": "/movie/static/css/main.baa8ff47.chunk.css"
+  },
+  {
+    "revision": "84568dda24b771ddfd03",
+    "url": "/movie/static/js/2.cfbf81ea.chunk.js"
+  },
+  {
+    "revision": "b5f406da65954719aa152e7e87278cdf",
+    "url": "/movie/static/js/2.cfbf81ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4900e10750fb74d6d713",
+    "url": "/movie/static/js/main.8690b9f8.chunk.js"
+  },
+  {
+    "revision": "03e15b226c9cfbe83dbe",
+    "url": "/movie/static/js/runtime-main.1c86db33.js"
+  }
+]);
